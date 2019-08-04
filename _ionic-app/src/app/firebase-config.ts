267bc35138017622cd457ced82b183efc2cfec5c
HervalMata <hervalmataamparo@gmail.com>
2019-08-04 18:40:34 -0300

    Configuracao do metodo de autenticacao por telefone

---
+++ _ionic-app/src/app/firebase-config.ts
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyC4UMGy_fCxTgleVWfAOinmmzGPubdiOCo",
+    authDomain: "troca-troca-b9507.firebaseapp.com",
+    databaseURL: "https://troca-troca-b9507.firebaseio.com",
+    projectId: "troca-troca-b9507",
+    storageBucket: "",
+    messagingSenderId: "478927477002",
+    appId: "1:478927477002:web:57bec6ae0de65320"
+};
