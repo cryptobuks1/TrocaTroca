b5bee32310aec4258b91502153867c05a76368f5
HervalMata <hervalmataamparo@gmail.com>
2019-08-04 22:11:35 -0300

    Redirecionamento para a area principal

--- _ionic-app/src/pages/login-phone-number/login-phone-number.ts
+++ _ionic-app/src/pages/login-phone-number/login-phone-number.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {FirebaseAuthProvider} from "../../providers/auth/firebase-auth";
 import {AuthProvider} from "../../providers/auth/auth";
+import {MainPage} from "../main/main";
 
 /**
  * Generated class for the LoginPhoneNumberPage page.
@@ -26,11 +27,28 @@ export class LoginPhoneNumberPage {
   }
 
   ionViewDidLoad() {
-      this.firebaseAuth.getToken().then((token) => {
-          console.log(token), (error) => console.log(error);
+      const unsubscribed = this.firebaseAuth.firebase.auth().onAuthStateChanged((user) => {
+          if (user) {
+              this.authService
+                  .login()
+                  .subscribe((token) => {
+                      this.redirectToMainPage();
+                  }, (responseError) => {
+                      this.redirectToCustumerCreatePage();
+                  });
+              unsubscribed();
+          }
       });
       this.firebaseAuth.makePhoneNumberForm('#firebase-ui');
       this.authService.login().subscribe((token) => console.log(token));
   }
+
+    redirectToMainPage() {
+        this.navCtrl.setRoot(MainPage);
+    }
+
+    redirectToCustumerCreatePage() {
+
+    }
 
 }
