d0002315ad2a0e1d95f03b2edbc459705b74dcd7
HervalMata <hervalmataamparo@gmail.com>
2019-08-05 17:51:22 -0300

    Criando cliente pelo ionic

--- _ionic-app/src/pages/customer-create/customer-create.ts
+++ _ionic-app/src/pages/customer-create/customer-create.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {CustomerHttpProvider} from "../../providers/customer-http/customer-http";
 
 /**
  * Generated class for the CustomerCreatePage page.
@@ -23,11 +24,13 @@ export class CustomerCreatePage {
     constructor(
         public navCtrl: NavController,
         public navParams: NavParams,
-        private formBuilder: FormBuilder
+        private formBuilder: FormBuilder,
+        private  customerHttp: CustomerHttpProvider
     ) {
         this.form = this.formBuilder.group({
             username: ['', [Validators.required, Validators.maxLength(30)]],
-            email: ['', [Validators.required, Validators.email]]
+            email: ['', [Validators.required, Validators.email]],
+            photo: null
         });
     }
 
@@ -36,13 +39,23 @@ export class CustomerCreatePage {
     }
 
     submit() {
-
+        this.customerHttp.create(this.form.value)
+            .subscribe(() => {
+                console.log('cliente foi criado.');
+            })
     }
 
     selectPhoto() {
         const nativeElement = this.inputFilePhoto.getElementRef().nativeElement;
         const inputFile = nativeElement.querySelector('input');
         inputFile.click();
+    }
+
+    onChoosePhoto(files: FileList) {
+        if (!files.length) {
+            return;
+        }
+        this.form.get('photo').setValue(files[0]);
     }
 
 }
