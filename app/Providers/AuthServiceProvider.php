c942dced9171b627a7d68f161b842db7237f22e6
HervalMata <hervalmataamparo@gmail.com>
2019-08-04 22:28:27 -0300

    Protegendo API administrativa

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace TrocaTroca\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use TrocaTroca\Models\User;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'TrocaTroca\Model' => 'TrocaTroca\Policies\ModelPolicy',
+
     ];
 
     /**
@@ -25,6 +27,20 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        \Gate::define('is_admin', function ($user) {
+            return $user->role = User::ROLE_ADMIN;
+        });
+
+        \Gate::define('is_manager', function ($user) {
+            return $user->role = User::ROLE_MANAGER;
+        });
+
+        \Gate::define('is_super', function ($user) {
+            return $user->role = User::ROLE_SUPER;
+        });
+
+        \Gate::define('is_operator', function ($user) {
+            return $user->role = User::ROLE_OPERATOR;
+        });
     }
 }
